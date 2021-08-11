e14aa62df2d616f3e91d0c25dc1f7cc590298211
Ivan Liu Nardo Shiddiq <48781708+ivanliunardo@users.noreply.github.com>
2021-08-11 16:30:04 +0700

    Update TravelWays.js
    Consume API ready to '/travelways'

--- src/components/TravelWays.js
+++ src/components/TravelWays.js
@@ -1,57 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import 'aos/dist/aos.css';
 import './TravelWays.css';
 import CardItem from './CardItem';
-import axios from 'axios';
+import {axios} from './axios';
 
-class TravelWays extends React.Component {
-  state = {
-    title: "Cara Pergi Ke Pulau Bawean",
-    cards: [
-      {
-        img: "/images/img-9.jpg",
-        text: "Exercitation duis nostrud non amet aliqua occaecat nulla duis consequat et."
-      },
-      {
-        img: "/images/img-2.jpg",
-        text: "Occaecat ea et esse est cupidatat esse non minim sint."
-      },
-      {
-        img: "/images/img-4.jpg",
-        text: "Irure laborum nulla Lorem pariatur enim elit aute Lorem laborum dolor nulla ea commodo."
-      }
-    ]
-  }
+function TravelWays(){
+  const [travelways, settravelWays] = useState([]);
 
-  componentDidMount(){
-    axios.get('/ways').then(res => {
-      this.setState(
-        {
-          title: res.data.title,
-          cards: res.data.cards
-        }
-      );
-    })
-  }
-  render(){
+    useEffect(() =>{
+        axios
+        .get("/travelways")
+        .then(response => {
+            console.log("Response:", response)
+            settravelWays(response.data)
+        })
+        .catch((err) => {
+            console.log("Error:", err)
+        })
+    }, []);
+
     return (
-      <div className='cards'>
-        <h1 data-aos='fade-up'>{this.state.title}</h1>
-        <div className='cards__container'>
-          <div className='cards__wrapper'>
-            <ul data-aos='fade-up' className='cards__items'>
-              {this.state.cards.map(ways => (
-                <CardItem
-                src= {ways.img}
-                text={ways.text}
-              />
-              ))}
-            </ul>
-          </div>
+      <div>
+        {
+          travelways.map(ways=>
+            <div className='cards'>
+              <h1 data-aos='fade-up'>{ways.title}</h1>
+              <div className='cards__container'>
+                <div className='cards__wrapper'>
+                <ul data-aos='fade-up' className='cards__items'>
+                {
+                  ways.cards.map(way => (
+                    <CardItem
+                    src= {way.img}
+                    text={way.text}
+                  />
+                ))}
+                </ul>
+                </div>
+              </div>
+            </div>
+            )
+          }
         </div>
-      </div>
     );
   }
-}
 
 export default TravelWays;
