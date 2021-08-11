da673e932a57245ffce8335569a71385da8437d1
Ivan Liu Nardo Shiddiq <48781708+ivanliunardo@users.noreply.github.com>
2021-08-11 16:22:29 +0700

    Update FAQ.js
    Consume API ready to '/faqs'

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,54 +1,48 @@
-import React from 'react';
+import React , {useState, useEffect} from "react";
 import 'aos/dist/aos.css'
 import '../App.css';
-import './FAQ.css'
-import axios from 'axios';
+import './FAQ.css';
+import {axios} from './axios';
 
-class FreqAskQue extends React.Component {
-    state = {
-        faqs: [
-            {
-                question: "Non aliquip ullamco fugiat voluptate dolore nisi ad.",
-                answer: "Cillum qui quis amet commodo irure Lorem voluptate. Sit ipsum elit ipsum ullamco sunt quis ullamco. Deserunt occaecat enim non nisi voluptate pariatur fugiat est cupidatat aute quis cupidatat sunt incididunt. Ea excepteur ut laborum nisi do exercitation pariatur labore quis. Labore ullamco deserunt ut deserunt. Tempor nulla nisi reprehenderit minim consequat nisi dolor reprehenderit quis eiusmod cillum enim. Dolore ullamco fugiat enim laborum exercitation.",
-            },
-            {
-                question: "Veniam aute sunt adipisicing proident Lorem consequat esse.",
-                answer: "Occaecat cupidatat ut amet dolor velit duis Lorem. Sunt cupidatat eu veniam pariatur veniam amet quis et elit in enim incididunt. Ut eiusmod deserunt labore veniam qui est sit adipisicing voluptate eu ad labore. Adipisicing ullamco id et ad in culpa voluptate exercitation. Sint tempor ea pariatur occaecat laboris exercitation et elit do aliquip cillum Lorem cupidatat reprehenderit. Tempor sunt in aute non sint non culpa sit labore reprehenderit commodo. Aute ex anim quis laborum tempor ex eiusmod ad nulla in excepteur."
-            }
-        ],
-        title: "Frequently Asked Question"
-    }
-        
+function FrequentlyAskedQuestions() {
+    const [faqs, setFaqs] = useState([]);
 
-    componentDidMount() {
-        axios.get('/faq').then(res => {
-            this.setState(
-                {
-                    faqs: res.data.faqs,
-                    title: res.data.title
-                }
-            );
+    useEffect(() => {
+        axios
+        .get("/faqs")
+        .then(response => {
+            console.log("Response:", response)
+            setFaqs(response.data)
         })
-    }
+        .catch((err) => {
+            console.log("Error:", err)
+        })
+    }, []);
 
-    render() {
-    
-        return(
-            <div className='faq-container'>
-                <div data-aos='fade-up' className='faq-title'>
-                    <h1> {this.state.title}</h1>
-                </div>
-                <div className='faq-content'>
-                    {this.state.faqs.map(faq => (
-                        <div>
-                            <h2 data-aos='fade-up'>{faq.question}</h2>
-                            <h3 data-aos='fade-up'>{faq.answer}</h3>
-                        </div>
-                    ))}
+    return(
+    <div>
+        <div className='faq-container'>
+            <div data-aos='fade-up' className='faq-title'>
+                <h1> Frequently Asked Questions!</h1>
+            </div>
+            <div className='faq-content'>
+                <div>
+                    <ul>
+                        {
+                            faqs.map(faq =>
+                            <div key={faq.id}>
+                                <h2 data-aos='fade-up'>{faq.question}</h2>
+                                <h3 data-aos='fade-up'>{faq.answer}</h3>
+                            </div>
+                            )
+                        }
+                    </ul>
                 </div>
             </div>
-        )
-    }
+        </div>
+    </div>
+    )
+
 }
 
-export default FreqAskQue
+export default FrequentlyAskedQuestions
