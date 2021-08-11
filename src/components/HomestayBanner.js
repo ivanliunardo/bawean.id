9b5a9b2745e79513aa1ef513b53b3367842b72d6
Ivan Liu Nardo Shiddiq <48781708+ivanliunardo@users.noreply.github.com>
2021-08-11 16:27:36 +0700

    Update HomestayBanner.js
    Consume API ready to '/homestay'

--- src/components/HomestayBanner.js
+++ src/components/HomestayBanner.js
@@ -1,57 +1,56 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import 'aos/dist/aos.css';
 import '../App.css';
 import { Button } from './Button';
 import './HomestayBanner.css';
-import axios from 'axios';
+import {axios} from './axios';
 
-class Homestay extends React.Component{
-    state = {
-        title: "Homestay Khas Bawean",
-        img: "/images/img-5.jpg",
-        text: "In ad Lorem in ut. Elit reprehenderit culpa excepteur pariatur eu et excepteur ex pariatur dolor sit laboris. Magna ipsum cillum adipisicing velit qui adipisicing tempor eiusmod cillum dolore. Fugiat et Lorem amet veniam ut non ipsum amet irure id id ullamco. Duis aliqua sint sint commodo consectetur officia ad. Occaecat occaecat proident commodo aliqua deserunt ipsum. Deserunt nulla adipisicing culpa laboris aute ipsum pariatur veniam."
-    }
+function Homestay(){
+    const [homestay, setHomestay] = useState([]);
 
-    componentDidMount() {
-        axios.get('/homestay').then(res => {
-            this.setState(
-                {
-                    title: res.data.title,
-                    img: res.data.img,
-                    text: res.data.text
-                }
-            );
+    useEffect(() =>{
+        axios
+        .get("/homestay")
+        .then(response => {
+            console.log("Response:", response)
+            setHomestay(response.data)
         })
-    }
-    
-    render(){
-        return(
-            <div className='homestay'>
-                <div data-aos='fade-up' className='homestay-title'>
-                    <h1>{this.state.title}</h1>
-                </div>
-                <div className='homestay-container'>
-                    <div data-aos='fade-right' className='homestayimg-container'>
-                        <img src={this.state.img} />
+        .catch((err) => {
+            console.log("Error:", err)
+        })
+    }, []);
+
+    return(
+        <div className='homestay'>
+            {
+                homestay.map(hmsty =>
+                    <div>
+                    <div data-aos='fade-up' className='homestay-title'>
+                        <h1>{hmsty.title}</h1>
                     </div>
-                    <div data-aos='fade-left' className='text-container'>
-                        <div className='text-content'>
-                            <h3>{this.state.text}</h3>
+                    <div className='homestay-container'>
+                        <div data-aos='fade-right' className='homestayimg-container'>
+                            <img src={hmsty.img} />
                         </div>
-                        <div className='homestay-btn'>
+                        <div data-aos='fade-left' className='text-container'>
+                            <div className='text-content'>
+                                <h3>{hmsty.text}</h3>
+                            </div>
+                            <div className='homestay-btn'>
                             <Button
                             className='btns'
                             buttonStyle='btn--primary'
-                            buttonSize='btn--large'
-                            onClick={console.log('hey')}>
+                            buttonSize='btn--large'>
                                 Pesan Sekarang
                             </Button>
-                        </div>
+                             </div>
+                         </div>
                     </div>
-                </div>
+                    </div>
+                )
+            }
             </div>
-        )
-    }
+    )
 }
 
 export default Homestay
