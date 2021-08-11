33654f1bb7900a1e6656774c5124f809d9ad74d3
Ivan Liu Nardo Shiddiq <48781708+ivanliunardo@users.noreply.github.com>
2021-08-11 16:21:29 +0700

    Update Demografi.js
    Consume API ready to '/demografi'

--- src/components/Demografi.js
+++ src/components/Demografi.js
@@ -1,40 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'aos/dist/aos.css'
 import '../App.css';
 import './Demografi.css';
-import axios from 'axios';
+import {axios} from './axios';
 
-class Demografi extends React.Component{
-    state = {
-        video: "/videos/map.mp4",   
-        title: "Demografi Pulau Bawean",
-        text: "Veniam magna ipsum proident labore. Lorem ad sint anim minim. Ipsum excepteur labore labore ex occaecat minim. Labore exercitation deserunt elit quis commodo esse laborum amet non. Cillum fugiat aliqua officia occaecat minim officia eu ipsum culpa. Ea incididunt labore culpa tempor eu ea occaecat minim qui do non duis eiusmod pariatur."
-    }
+function Demografi() {
+    const [demografi, setDemografi] = useState([]);
 
-    componentDidMount(){
-        axios.get('/map').then(res =>{
-            this.setState(
-                {
-                    video: res.data.video,
-                    title: res.data.title,
-                    text: res.data.text
-                }
-            );
+    useEffect(() =>{
+        axios
+        .get("/demografi")
+        .then(response => {
+            console.log("Response:", response)
+            setDemografi(response.data)
         })
-    }
-    render(){
-        return(
-            <div className='mapdesc-container'>
-                <div data-aos='fade-right'  className='map'>
-                    <video src={this.state.video} autoPlay loop muted />
-                </div>
-                <div  data-aos='fade-left'  className='desc'>
-                    <h1 class='title'>{this.state.title}</h1>
-                    <h3>{this.state.text}</h3>
-                </div>
-            </div>
-        )
-    }
+        .catch((err) => {
+            console.log("Error:", err)
+        })
+    }, []);
+
+    return(
+        <div>
+            {
+                demografi.map(dem =>
+                    <div key={dem.id}>
+                        <div className='mapdesc-container'>
+                            <div data-aos='fade-right'  className='map'>
+                                <video src={dem.video} autoPlay loop muted />
+                            </div>
+                            <div  data-aos='fade-left'  className='desc'>
+                                <h1 class='title'>{dem.title}</h1>
+                                <h3>{dem.text}</h3>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+        </div>
+    )
 }
 
 export default Demografi
