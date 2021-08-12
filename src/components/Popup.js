5988e6fd9f3c089a7e7e290ae0ebf5b0f6c40be8
Ivan Liu Nardo Shiddiq <48781708+ivanliunardo@users.noreply.github.com>
2021-08-12 14:14:37 +0700

    Update Popup.js

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,11 +1,12 @@
 import React from "react";
 import './Popup.css';
+import CloseIcon from "@material-ui/icons/Close";
 
 const Popup = props => {
   return (
     <div className="popup-box">
       <div className="box">
-        <span className="close-icon" onClick={props.handleClose}>x</span>
+        <span className="close-icon" onClick={props.handleClose}><CloseIcon/></span>
         {props.content}
       </div>
     </div>
